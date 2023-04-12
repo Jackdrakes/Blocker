42330418f9fd3b889108521541637097cc3a5b86
Jackdrakes <113877112+Jackdrakes@users.noreply.github.com>
2023-04-12 23:48:06 +0530

    Add files via upload

---
+++ bg.js
@@ -0,0 +1,11 @@
+export default function UrlArr() {
+    const arrurl = ["www.youtube.com",
+    "www.facebook.com",
+    "www.netflix.com",
+    "www.instagram.com",
+    "discord.com",
+    "www.spotify.com",
+    "open.spotify.com"]
+
+    return  arrurl
+}
